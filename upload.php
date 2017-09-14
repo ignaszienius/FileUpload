45c9dad91d65b628f8e52f16c38b849814533525
ignaszienius <ignaszienius@gmail.com>
2017-09-14 19:39:10 +0300

    keiciam dar nauja failo pavadinimas veikia vikas

--- upload.php
+++ upload.php
@@ -7,6 +7,8 @@ if(isset($_FILES['file'])) {
     $file_tmp = $file['tmp_name'];
     $file_size = $file['size'];
     $file_error = $file['error'];
+    $file_newname = $_POST['newname'];
+
 
     //Nustatom tipa failo
     $file_ext = explode('.', $file_name);
@@ -20,13 +22,15 @@ if(isset($_FILES['file'])) {
         if($file_error === 0) {
             if($file_size <= 1048576) {
 
+                $file_name = $file_newname . "." . $file_ext ;
                 $file_destination = "uploads/" . $file_name ;
+
                 if(move_uploaded_file($file_tmp, $file_destination)) {
-                    echo "Failas sėkmingai įkeltas į šią direktoriją: " . $file_destination;
+                    echo "Failo pavadinimas sėkmingai pakeistas ir įkeltas į šią direktoriją: " . $file_destination;
                 }
             }
         }
     }
 
-
+print_r($_FILES);
 }
